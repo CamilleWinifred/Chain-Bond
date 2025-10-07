f3a87b91d8b8e45a65197c8383a73ea0d0664b18
Dominic-Margery <qukahasakuno@gmail.com>
2025-10-07 12:39:18 +0800

    Add addresses mapping

---
+++ frontend/abi/ChainBondAddresses.ts
@@ -0,0 +1,6 @@
+
+/* Auto-generated */
+export const ChainBondAddresses = {
+  "11155111": { address: "0x6962b2990a3FD1CE5ed0750D644fbDA4203d3e01", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" }
+};
